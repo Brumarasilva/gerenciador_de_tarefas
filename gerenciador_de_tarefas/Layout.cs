e3222d2fb7c23af12f552a0c9ed9387a8d4c7266
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-03-18 10:40:33 -0300

    comentando a aba layout do gerenciar_tarefa

--- gerenciador_de_tarefas/Layout.cs
+++ gerenciador_de_tarefas/Layout.cs
@@ -4,14 +4,14 @@ namespace Layout
 {
     class Formatacao
     {
-        public static void Cor(string mensagem, ConsoleColor cor)
+        public static void Cor(string mensagem, ConsoleColor cor)//ao criar esse metódo formatacao.Cor quando o usuário quiser mudar a cor de uma descrição ele vai usar esse metódo ao inves de utilizar Console.WriteLine.
         {
             Console.ForegroundColor = cor;
             Console.WriteLine(mensagem);
             Console.ResetColor();
         }
 
-        public static void ImprimirCabecalho()
+        public static void ImprimirCabecalho()//Esse cabeçalho vai ser impresso quando o programa estiver rodando.
         {
             Console.WriteLine("╔════════════════════════╗");
             Console.WriteLine("║ GERENCIADOR DE TAREFAS ║");
