06968b99187fefc564487a29a860474437b1421e
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-03-18 10:50:18 -0300

    comentando tarefas.cs

--- gerenciador_de_tarefas/Tarefas.cs
+++ gerenciador_de_tarefas/Tarefas.cs
@@ -5,16 +5,16 @@ namespace Tarefas
     class Tarefa
 
     {
-         public int Id{get; set;}
+         public int Id{get; set;}//Um número inteiro que identifica a tarefa de forma única. Cada tarefa tem um ID diferente.
 
-        public string Descricao {get; set;}
-        public bool Concluida {get; set;}
+        public string Descricao {get; set;}// Um texto que descreve o que a tarefa é (por exemplo, "Estudar C#").
+        public bool Concluida {get; set;}//Um valor booleano (true ou false) que indica se a tarefa foi concluída ou não.
        
-       public Tarefa (int id, string descricao)
+       public Tarefa (int id, string descricao)//é um método especial usado para criar um objeto da classe Tarefa. Ele define os valores iniciais para as propriedades Id e Descricao quando a tarefa é criada.
        {
-        Id = id;
-        Descricao = descricao;
-        Concluida = false;
+        Id = id;//Inicializam as propriedades com os valores passados quando a tarefa for criada.
+        Descricao = descricao;//Inicializam as propriedades com os valores passados quando a tarefa for criada.
+        Concluida = false;//Define que a tarefa começa como não concluída (false).
        }
        
 
